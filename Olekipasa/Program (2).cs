b7e8a6a1adbdd85c0ff6cd3dda14f5ed4ad97ce2
dababycar1337 <157720797+dababycar1337@users.noreply.github.com>
2024-02-07 09:23:06 +0300

    Update Program (2).cs

--- Olekipasa/Program (2).cs
+++ Olekipasa/Program (2).cs
@@ -12,7 +12,7 @@ namespace App1337
         {
             Console.WriteLine("Всё про одномерные массивы:");
             Console.WriteLine("Введите количество элементов массива");
-            First fr = new First(int.Parse(Console.ReadLine()));
+            First fr = new First(3);
             fr.MakeArray();
             fr.FindMiddle();
             fr.WriteAll();
@@ -22,7 +22,7 @@ namespace App1337
 
             Console.WriteLine("Всё про двумерные массивы:");
             Console.WriteLine("Введите количество строк и стобцов двумерного массива");
-            Second sc = new Second(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Second sc = new Second(3, 3);
             sc.MakeArray();
             sc.FindMiddle();
             sc.WriteAll();
@@ -32,7 +32,7 @@ namespace App1337
             
             Console.WriteLine("Всё про ступенчатые массивы:");
             Console.WriteLine("Введите количество строк ступенчатого массива");
-            Third th = new Third(int.Parse(Console.ReadLine()));
+            Third th = new Third(3);
             Console.WriteLine("Создайте ступенчатый массив по своим критериям");
             th.MakeArray();
             th.FindMiddle();
