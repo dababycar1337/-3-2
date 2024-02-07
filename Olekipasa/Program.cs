80a6fdf160c6869686bac2af73589c27bf1a30e7
dababycar1337 <157720797+dababycar1337@users.noreply.github.com>
2024-02-07 10:07:40 +0300

    Update Program.cs

--- Olekipasa/Program.cs
+++ Olekipasa/Program.cs
@@ -10,29 +10,31 @@ namespace App1337
     {
         static void Main()
         {
+            ForAll[] arrays = new ForAll();
+            arrays[0] = new First(3);
+            arrays[1] = new Second(3, 3);
+            arrays[2] = new Third(3);
+
             Console.WriteLine("Всё про одномерные массивы:");
-            First fr = new First(3);
-            fr.MakeArray();
-            fr.FindMiddle();
-            fr.WriteAll();
+            arrays[0].Makearray();
+            arrays[0].FindMiddle();
+            arrays[0].WriteAll();
             Console.WriteLine("");
             Console.WriteLine("");
 
 
             Console.WriteLine("Всё про двумерные массивы:");
-            Second sc = new Second(3, 3);
-            sc.MakeArray();
-            sc.FindMiddle();
-            sc.WriteAll();
+            array[1].MakeArray();
+            array[1].FindMiddle();
+            array[1].WriteAll();
             Console.WriteLine("");
             Console.WriteLine("");
 
             
             Console.WriteLine("Всё про ступенчатые массивы:");
-            Third th = new Third(3);
-            th.MakeArray();
-            th.FindMiddle();
-            th.WriteAll();
+            array[2].MakeArray();
+            array[2].FindMiddle();
+            array[2].WriteAll();
         }
     }
 }
